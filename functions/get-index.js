9a4be0a9b07bd41991214d4e1bef18a270422861
Greg Campion <greg.campion@paessler.com>
2020-09-06 14:59:51 +0200

    cognito auth for search

--- functions/get-index.js
+++ functions/get-index.js
@@ -1,21 +1,18 @@
-const fs = require('fs')
+const fs = require("fs")
 const Mustache = require('mustache')
 const http = require('axios')
 const aws4 = require('aws4')
 const URL = require('url')
 
 const restaurantsApiRoot = process.env.restaurants_api
-const days = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday'
-]
+const cognitoUserPoolId = process.env.cognito_user_pool_id
+const cognitoClientId = process.env.cognito_client_id
+const awsRegion = process.env.AWS_REGION
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
 const template = fs.readFileSync('static/index.html', 'utf-8')
+
 const getRestaurants = async () => {
     console.log(`loading restaurants from ${restaurantsApiRoot}...`)
     const url = URL.parse(restaurantsApiRoot)
@@ -36,7 +33,15 @@ module.exports.handler = async (event, context) => {
     const restaurants = await getRestaurants()
     console.log(`found ${restaurants.length} restaurants`)
     const dayOfWeek = days[new Date().getDay()]
-    const html = Mustache.render(template, { dayOfWeek, restaurants })
+    const view = {
+        awsRegion,
+        cognitoUserPoolId,
+        cognitoClientId,
+        dayOfWeek,
+        restaurants,
+        searchUrl: `${restaurantsApiRoot}/search`
+    }
+    const html = Mustache.render(template, view)
     return {
         statusCode: 200,
         headers: {
